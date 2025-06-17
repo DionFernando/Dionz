8f870b16944b9f51076b29cf144cab4c45404ff6
DionFernando <dionfernando2003@gmail.com>
2025-06-17 22:27:23 +0530

    intial commit

---
+++ src/main/java/lk/dionz/dionz/DionzApplication.java
@@ -0,0 +1,13 @@
+package lk.dionz.dionz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/dionz/dionz/DionzApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dionz.dionz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
